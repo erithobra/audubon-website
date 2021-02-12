d3173bdda832462a83960573fed99d6549838c8b
ericb2020 <ebrauer83@gmail.com>
2021-02-12 01:39:16 -0600

    i guess everything is working correctly now

--- audubon-app/src/components/Show.js
+++ audubon-app/src/components/Show.js
@@ -6,11 +6,15 @@ class Show extends Component {
 
         const birdy = this.props.match.params.birdIndex;
 
-        console.log(this.props.birds[birdy]);
+        console.log(this.props.birds[birdy].name);
         
         return (
             <div>
+                <p>{this.props.birds[birdy].name}</p>
                 <img src={this.props.birds[birdy].image}></img>
+                <p>{this.props.birds[birdy].genus}</p>
+                <p>{this.props.birds[birdy].conservationStatus}</p>
+                <p>{this.props.birds[birdy].homepage}</p>
 
 
             </div>
